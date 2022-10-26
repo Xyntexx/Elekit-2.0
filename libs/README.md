c46a6931c9e3a15f8cadbf5c2f71245be50efa05
Xyntexx <mnuuja@gmail.com>
2022-10-26 06:50:26 +0300

    Add a workflow to import JLCPCB symbols and footprints. Add symbols and footprints for components in schematic

---
+++ libs/README.md
@@ -0,0 +1,9 @@
+Use create_libaries.py to download and create symbols and footprints for JLCPCB components.
+
+Uses [](https://github.com/TousstNicolas/JLC2KiCad_lib)
+
+Make sure git submodules are initialized!
+
+### Dependencies:
+`pip install requests`
+`pip install KicadModTree`
---
+++ libs/create_libaries.py
@@ -0,0 +1,19 @@
+import sys
+
+from JLC2KiCad_lib.JLC2KiCadLib import JLC2KiCadLib
+
+# This is the list of the parts that you want to add to the library
+component_list = [
+    "C2765186",  # USB Type-C Connector
+    "C2934560",  # ESP32-C3-WROOM-02-N4
+    "C318884",  # NO Push Button
+
+]
+
+argumets = "-dir ./ -schematic_lib 0_JLCPCB_Symbols -footprint_lib 0_JLCPCB_Footprints" \
+    .split(" ")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    sys.argv = ["JLC2KiCadLib", *component_list, *argumets]
+    JLC2KiCadLib.main()
